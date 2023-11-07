20457493dfab2aa652141bb18a444817ddc1b8e4
Ben Brittle <85850476+BenBrittle@users.noreply.github.com>
2023-11-07 20:51:10 +0000

    build created

--- build/temp.linux-x86_64-cpython-310/python/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/YDLidar-SDK/python
-# Build directory: /workspaces/YDLidar-SDK/build/temp.linux-x86_64-cpython-310/python
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ydlidar_py_test "/opt/python/3.10.8/bin/python3.10" "/workspaces/YDLidar-SDK/python/test/pytest.py")
-set_tests_properties(ydlidar_py_test PROPERTIES  ENVIRONMENT "PYTHONPATH=:/workspaces/YDLidar-SDK/build/temp.linux-x86_64-cpython-310/python" _BACKTRACE_TRIPLES "/workspaces/YDLidar-SDK/python/CMakeLists.txt;42;add_test;/workspaces/YDLidar-SDK/python/CMakeLists.txt;0;")
-subdirs("examples")
--- build/temp.linux-x86_64-cpython-310/python/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/YDLidar-SDK/python/examples
-# Build directory: /workspaces/YDLidar-SDK/build/temp.linux-x86_64-cpython-310/python/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
